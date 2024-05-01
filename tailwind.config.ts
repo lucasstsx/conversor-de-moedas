43252e82fa73d5f4229dae5b79890767198c45a6
Lucas Silva <lucas.scluiz@gmail.com>
2024-05-01 12:59:01 -0300

    fix: fix theme colors in tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,18 +10,6 @@ const config = {
   ],
   prefix: '',
   theme: {
-    colors: {
-      'blue-100': '#080815',
-      'blue-200': '#0E0F25',
-      'blue-300': '#141534',
-      'blue-400': '#1F2151',
-      'blue-500': '#2F3279',
-      'blue-600': '#2F34AB',
-      'blue-700': '#4A5DCD',
-      'blue-800': '#7D8DEC',
-      'blue-900': '#B2B8DE',
-      'whit-e': '#F1F2F6',
-    },
     container: {
       center: true,
       padding: '2rem',
@@ -43,6 +31,18 @@ const config = {
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
+      },
+      colors: {
+        'blue-100': '#080815',
+        'blue-200': '#0E0F25',
+        'blue-300': '#141534',
+        'blue-400': '#1F2151',
+        'blue-500': '#2F3279',
+        'blue-600': '#2F34AB',
+        'blue-700': '#4A5DCD',
+        'blue-800': '#7D8DEC',
+        'blue-900': '#B2B8DE',
+        white: '#F1F2F6',
       },
     },
   },
